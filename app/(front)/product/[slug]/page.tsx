e47c3a07b14c88bff4e830c8a0dcd19f6987d6f0
pratik <pratikpoudel9800@gmail.com>
2025-01-07 15:04:03 +0545

    two changed

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,29 +1,29 @@
-import { Button } from '@/components/ui/button';
-import productsService from '@/services/ProductServices';
-import Link from 'next/link';
-import React from 'react';
+// import { Button } from '@/components/ui/button';
+// import productsService from '@/services/ProductServices';
+// import Link from 'next/link';
+// import React from 'react';
 
-interface ProductDetailsProps {
-  params: {
-    slug: string;
-  };
-}
+// interface ProductDetailsProps {
+//   params: {
+//     slug: string;
+//   };
+// }
 
-const ProductDetails = async({params}:ProductDetailsProps) => {
-  const product = await productsService.getBySlug(params.slug);
-  // If the product is not found
-  if (!product) {
-    return <div>Product not found</div>;
-  }
+// const ProductDetails = async({params}:ProductDetailsProps) => {
+//   const product = await productsService.getBySlug(params.slug);
+//   // If the product is not found
+//   if (!product) {
+//     return <div>Product not found</div>;
+//   }
 
-  return (
-    <div>
-      <Button className="my-2">
-        <Link href="/games">Back to Games</Link>
-      </Button>
-      <h1>{product.name}</h1>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <Button className="my-2">
+//         <Link href="/games">Back to Games</Link>
+//       </Button>
+//       <h1>{product.name}</h1>
+//     </div>
+//   )
+// }
 
-export default ProductDetails
+// export default ProductDetails
