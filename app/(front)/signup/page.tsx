8704cb69eb4b8a38706b06d9777c6504b4d5abb9
pratik <pratikpoudel9800@gmail.com>
2025-01-08 13:46:13 +0545

    changed

--- app/(front)/signup/page.tsx
+++ app/(front)/signup/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { getServerSession } from 'next-auth'
 import {redirect} from 'next/navigation'
-import {authOptions} from '@/app/api/auth/[...nextauth]/route'
 import SignupForm from '@/components/signupForm'
+import { config } from '@/app/api/auth/[...nextauth]/route'
 
 const Signup = async () => {
 
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(config)
 
   if(session) redirect("/games")
 
